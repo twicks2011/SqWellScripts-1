c9b6894b538ee7c81987fc7953af36eb48e79a50
RichGraham <Richard.Graham@nottingham.ac.uk>
2016-07-26 12:13:52 +0100

    Implemented with proper output files from SqWell code

--- batchOutput/batchOutput.sh
+++ batchOutput/batchOutput.sh
@@ -6,10 +6,11 @@ for i in $(seq 1 $(expr ${#arr1[@]} - 1))
 do
    :
    echo ${arr1[$i]} ${arr2[$i]}
-   all="$all ${arr1[$i]}"
+   doBoth.sh ${arr1[0]} ${arr2[0]} inputFiles/${arr1[$i]}.dat ${arr2[$i]}
+   all="$all outputFiles/weightFiles/${arr1[$i]}.dat"
 done
 
 echo $all
-xmgrace $all
+xmgrace  -g 1200x900 $all &
 
 
