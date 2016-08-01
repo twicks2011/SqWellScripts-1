ee840c4fdabd983e26f8bb1053ca8be13c1d5dbb
RichGraham <Richard.Graham@nottingham.ac.uk>
2016-08-01 11:53:32 +0100

    makefiles: can now specify c compiler via ./makeAll.sh [c compiler]

--- makeAll.sh
+++ makeAll.sh
@@ -1,15 +1,38 @@
 #!/bin/sh
+#Copy the shell scripts to a folder in the path
 cp doBoth.sh ~/bin/
 cp showAll.sh ~/bin/
 cp sqWellCp.sh ~/bin/
 cp batchOutput/batchOutput.sh ~/bin/
 cp checkOutputFolder.sh ~/bin
 
+
+#Check correct numnber of command line arguments
+if (( $# > 1 )); then
+   echo "usage: makeAll.sh [c compiler choice: defaults to gcc if not specified]"
+  exit 1
+fi
+
+if (( $# < 1 )); then
+    compiler=gcc
+    echo "Defaulting to gcc compiler"
+fi
+
+
+
+#Check correct numnber of command line arguments
+if [[ $# -eq 1 ]]; then
+    compiler=$1
+   echo "Using compiler $compiler."
+fi
+
+
+
 echo ""
 for f in blockVary clipQuad collateClip dosMapping; do
     echo $f
     cd $f
-    make
+    make CC=$compiler
     cd ..
     echo ""
 done
