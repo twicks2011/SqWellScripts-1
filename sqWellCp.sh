dcc7f9f35eb991b4e2a3366f36c35196b74a61c8
RichGraham <Richard.Graham@nottingham.ac.uk>
2016-08-17 11:56:41 +0100

    SqWellCp now checks arguments and reports syntax if the user input is wrong

--- sqWellCp.sh
+++ sqWellCp.sh
@@ -1,3 +1,21 @@
+#!/bin/bash
+
+#Check correct numnber of command line arguments
+if (( $# != 2 )); then
+  echo "Usage:  sqWellCp.sh [orginal input file] [copied inputfile]" >&2
+  exit 1
+fi
+
+if [  -f $2 ]; then
+    echo "File $2 already exists: refusing to overwrite!!!! "
+    exit 1
+fi
+
+if [ ! -f $1 ]; then
+    echo "File $1 does not exist: can't continue!!! "
+    exit 1
+fi
+
 old=${1%.*}
 new=${2%.*}
 echo $old $new
