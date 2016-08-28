48caffd2a315f9588e75d1293b30620508fd9e78
Richard Graham <Richard.Graham@nottingham.ac.uk>
2016-08-28 13:15:29 +0100

    BatchOutput: Added a flag in 3rd column - if this is -1 then averaging is not redone

--- batchOutput/batchOutput.sh
+++ batchOutput/batchOutput.sh
@@ -1,13 +1,16 @@
 arr1=( $( cut -d' ' -f1 $1 ) )
 arr2=( $( cut -d' ' -f2 $1 ) )
-#I put a comment here
+arr3=( $( cut -d' ' -f2 $1 ) )
 
 
 for i in $(seq 1 $(expr ${#arr1[@]} - 1))
 do
    :
-   echo ${arr1[$i]} ${arr2[$i]}
-   doBoth.sh ${arr1[0]} ${arr2[0]} inputFiles/${arr1[$i]}.dat ${arr2[$i]}
+   echo ${arr1[$i]} ${arr2[$i]} ${arr3[$i]}
+   if [ "${arr3[$i]}" -ne "-1" ];
+   then
+       doBoth.sh ${arr1[0]} ${arr2[0]} inputFiles/${arr1[$i]}.dat ${arr2[$i]};
+   fi;
    all="$all outputFiles/weightFiles/${arr1[$i]}.dat"
 done
 
